55ac5734b2b256526f670db35cc7b90d7128abc9
Joey Rideout <joeyrrideout@gmail.com>
2014-11-20 23:46:50 -0500

    Updated animation.

--- js/shapes.js
+++ js/shapes.js
@@ -1,6 +1,8 @@
 var w = 400;
 var h = 400;
 var interval = w/4;
+var sw = 40;
+var sh = 40;
 
 var flow_shapes = { 
   prep: function(height, width) {
@@ -10,11 +12,15 @@ var flow_shapes = {
   io: function(height, width) {
     var points = [ [0,height], [width*.2,0], [width,0], [width*.8,height], [0,height] ]
     return d3.svg.line()(points);
+  },
+  square: function(height, width) {
+    var points = [ [0,height], [width,0], [width,0], [width,height], [0,height] ]
+    return d3.svg.line()(points);
   }
 }
 
 var nodes = [
-  {NodeType: "prep", x:50, y:0,  height:40, width: 40},
+  {NodeType: "prep", x:50, y:0,  height:sh, width: sw},
 ]
 
 svg = d3.select("#canvas").append("svg:svg").attr("width", w).attr("height", h)
@@ -28,27 +34,28 @@ svg.selectAll("path")
     return "translate(" + d.x + "," + d.y + ")"
   });
 
-function animate(x, y, colour) {
+function animate(x, y, colour, type) {
   d3.selectAll("path")
-    .transition()
+    .transition().delay(300)
     .attr("fill", colour)
+    .attr("d", function() { return flow_shapes[type](sh, sw);})
     .attr("transform", function(d) {
       return "translate(" + x + "," + y + ")"
     });
 }
 
 $('#about').click(function() {
-  animate(0, 0, "white");
+  animate(0, 0, "white", "square");
 });
 
 $('#blog').click(function() {
-  animate(interval, 0, "red");
+  animate(interval, 0, "red", "prep");
 });
 
 $('#contact').click(function() {
-  animate(interval*2, 0, "green");
+  animate(interval*2, 0, "green", "io");
 });
 
 $('#github').click(function() {
-  animate(interval*3, 0, "blue");
+  animate(interval*3, 0, "blue", "prep");
 });
